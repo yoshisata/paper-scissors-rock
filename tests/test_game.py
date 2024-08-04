9634ab62ad4cd0be022e6bcc9fc8a8e78b7b20af
satamana <sler4atamanov@gmail.com>
2024-08-04 11:01:39 +0200

    Refactor get_number_of_rounds method, adjust tests

--- tests/test_game.py
+++ tests/test_game.py
@@ -28,13 +28,21 @@ class TestPaperScissorsRockGame(unittest.TestCase):
             self.assertEqual(game.rounds[0].computer_choice, Choice.ROCK)
             self.assertEqual(game.rounds[0].result, RoundResult.DRAW)
 
-    @patch('builtins.input', side_effect=[1, 1])
+    @patch('builtins.input', side_effect=['1'])
     def test_get_number_of_rounds(self, mock_input):
         self.assertEqual(PaperScissorsRockGame.get_number_of_rounds(), 1)
 
-    @patch('builtins.input', side_effect=[11, 1])
+    @patch('builtins.input', side_effect=['11', 'y'])
     def test_get_number_of_rounds_confirm(self, mock_input):
         self.assertEqual(PaperScissorsRockGame.get_number_of_rounds(), 11)
+
+    @patch('builtins.input', side_effect=['11', 'n', '5'])
+    def test_get_number_of_rounds_reject(self, mock_input):
+        self.assertEqual(PaperScissorsRockGame.get_number_of_rounds(), 5)
+
+    @patch('builtins.input', side_effect=['invalid', '5'])
+    def test_get_number_of_rounds_invalid_input(self, mock_input):
+        self.assertEqual(PaperScissorsRockGame.get_number_of_rounds(), 5)
 
 
 if __name__ == '__main__':
